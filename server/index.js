2471aed27da296528549cd83913087f6a3bd9a0b
Molonlabee <janayahernandez@yahoo.com>
2020-01-29 23:42:51 -0600

    node-2

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mc = require("./controllers/message_controller");
+const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname + '/../public/build'));
+
+const messagesBaseUrl = "/api/messages";
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(messagesBaseUrl, mc.update);
+app.delete(messagesBaseUrl, mc.delete);
+
+const port = 3001;
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+});
+
