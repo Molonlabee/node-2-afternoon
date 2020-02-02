6c8bdd6497912b267839b71b2ad49646417b1b5c
Molonlabee <janayahernandez@yahoo.com>
2020-02-02 15:53:02 -0600

    node-2-afternoon

--- server/controllers/message_controller.js
+++ server/controllers/message_controller.js
@@ -10,13 +10,13 @@ module.exports = {
     },
 
     read: (req, res) => {
-        res.statues(200).send(messages);
+        res.status(200).send(messages);
     },
 
     update: (req, res) => {
         const {text} =req.body;
         const updateID = req.params.id;
-        const messageIndex = messages.findIndex(message => message.id == this.update.ID);
+        const messageIndex = messages.findIndex(message => message.id ==updateID);
        let message = messages[messageIndex];
     
        messages[messageIndex] = {
--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ app.get(messagesBaseUrl, mc.read);
 app.put(messagesBaseUrl, mc.update);
 app.delete(messagesBaseUrl, mc.delete);
 
+
+
 const port = 3001;
 app.listen(port, () => {
     console.log(`server listening on port ${port}`);
